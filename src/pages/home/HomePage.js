760786d2cfcd155bf2d5a4909dc8b9b7c06bc383
Leonardo W <47224043+leonw00@users.noreply.github.com>
2022-06-05 16:41:27 -0500

    Leo (#3)
    * added basic code for homepage
    
    * made basic submit form for testing logic
    
    Co-authored-by: Leonardo <Leonardo@Evelyns-MacBook-Air.local>

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,15 @@
-import './HomePage.css'
+import "./HomePage.css";
 
-function HomePage(){
-    return(
-        <div>
-            This is the homepage
-        </div>
-    );
+function HomePage() {
+  return (
+    <div>
+      <form>
+        <input placeholder="your location" type="text" />
+        <input placeholder="distance from your location" type="text" />
+        <input name="Submit" type="submit"/>
+      </form>
+    </div>
+  );
 }
 
 export default HomePage;
