be08e9077fbf1ef1a7ceb80b380664bfde40a7ef
Leonardo <Leonardo@Evelyns-MacBook-Air.local>
2022-06-15 14:41:30 -0500

    testing variables

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -6,8 +6,8 @@ const google = window.google;
 function HomePage() {
 
   useEffect(() => {
-    
     const sydney = new google.maps.LatLng(-33.867, 151.195);
+    console.log(sydney);
     console.log("TESTING");
   }, []);
 
