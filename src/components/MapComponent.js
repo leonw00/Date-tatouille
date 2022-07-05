8c139758f87113295a69c08e143400935ef4ce33
Leonardo <leonardow1908@gmail.com>
2022-07-04 20:58:30 -0500

    set current location as initial in map component

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,9 +1,5 @@
-import React, { useState } from "react";
-import {
-  GoogleMap,
-  InfoWindow,
-  Marker,
-} from "@react-google-maps/api";
+import React, { useEffect, useState } from "react";
+import { GoogleMap, InfoWindow, Marker } from "@react-google-maps/api";
 import { setMap } from "../backend/LogicCalls";
 
 // *******************************
@@ -11,19 +7,28 @@ import { setMap } from "../backend/LogicCalls";
 // display the map
 // *******************************
 const MapComponent = () => {
+  const [mapPosition, setMapPosition] = useState({});
+  const [activeInfoWindow, setActiveInfoWindow] = useState("");
+  const [markers, setMarkers] = useState(initialMarkers);
+
+  useEffect(() => {
+    var longitude = window.localStorage.getItem("current_longitude");
+    var latitude = window.localStorage.getItem("current_latitude");
+    if(longitude && latitude){
+      mapPosition = {
+        lat: latitude,
+        lng: longitude,
+      };
+    }
+  }, []);
+
   const initialMarkers = [
     {
-      position: {
-        lat: -33.8665433,
-        lng: 151.1956316,
-      },
+      position: mapPosition,
       label: { color: "white", text: "P1" },
       draggable: true,
     },
   ];
-
-  const [activeInfoWindow, setActiveInfoWindow] = useState("");
-  const [markers, setMarkers] = useState(initialMarkers);
 
   const containerStyle = {
     width: "100%",
