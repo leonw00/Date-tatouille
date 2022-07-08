5106ec6b84e768ade29aa1ea023aca06117449b3
wynonagcc <wynonagoldiella@gmail.com>
2022-07-08 03:17:09 -0500

    Fixed input selection styles

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -2,10 +2,12 @@ import { getCurrentLocation, nearbySearch } from "../../backend/LogicCalls";
 import MapComponent from "../../components/MapComponent";
 import { useEffect, useState } from "react";
 import "./HomePage.css";
+import mapMarkerIcon from "../../assets/icons/mapMarker.png";
 
 function HomePage() {
   const [location, setLocation] = useState("");
   const [range, setRange] = useState(50);
+  const [rangeSelection, setRangeSelection] = useState(50);
   const [indoor, setIndoor] = useState(true);
   const [outdoor, setOutdoor] = useState(false);
 
@@ -38,47 +40,61 @@ function HomePage() {
 
       <div className="input-container">
         <div className="address-input-container">
-          <i className="fa-solid fa-user"></i>
+          <img className="icon" src={mapMarkerIcon} alt="Map Marker Icon" />
           <input
-            id="location-input"
+            id="location-input" 
+            className="input"
             placeholder="Enter Your Location"
             type="text"
             value={location}
             onChange={(e) => setLocation(e.target.value)}
           />
         </div>
-
         <div className="map-placeholder"><MapComponent/></div>
-        <p>Range</p>
-        <input
-          type="range"
-          min="1"
-          max="100"
-          className="slider"
-          defaultValue={range}
-          onMouseUp={(e) => setRange(e.target.value)}
-        />
-        <div>
-          <p>Indoor</p>
-          <label className="switch">
-            <input
-              type="checkbox"
-              checked={indoor}
-              onChange={(e) => setIndoor(e.target.checked)}
-            />
-            <span className="slider-round"></span>
-          </label>
-        </div>
-        <div>
-          <p>Outdoor</p>
-          <label className="switch">
-            <input
-              type="checkbox"
-              checked={outdoor}
-              onChange={(e) => setOutdoor(e.target.checked)}
-            />
-            <span className="slider-round"></span>
-          </label>
+        <div className="input-sub-container">
+          <div className="left-wrapper">
+            <p className="input">Radius</p>
+            <p className="input">Indoor</p>
+            <p className="input">Outdoor</p>
+          </div>
+          <div className="right-wrapper">
+            <div className="input-selection">
+              <input
+                type="range"
+                min="1"
+                max="100"
+                className="slider"
+                defaultValue={range}
+                onMouseUp={(e) => setRange(e.target.value)}
+                onChange={(e) => setRangeSelection(e.target.value)}
+              />
+              <div>
+                <label className="switch">
+                  <input
+                    type="checkbox"
+                    checked={indoor}
+                    onChange={(e) => setIndoor(e.target.checked)}
+                  />
+                  <span className="slider-round"></span>
+                </label>
+              </div>
+              <div>
+                <label className="switch">
+                  <input
+                    type="checkbox"
+                    checked={outdoor}
+                    onChange={(e) => setOutdoor(e.target.checked)}
+                  />
+                  <span className="slider-round"></span>
+                </label>
+              </div>
+            </div>
+            <div className="input-description">
+              <p className="input">{rangeSelection} km</p>
+              <p className="input">{outputBoolean(indoor)}</p>
+              <p className="input">{outputBoolean(outdoor)}</p>
+            </div>
+          </div>
         </div>
         <button
           id="generate-button"
@@ -89,7 +105,6 @@ function HomePage() {
         >
           Generate Date Ideas!
         </button>
-        {/* <MapComponent /> */}
       </div>
     </div>
   );
@@ -108,6 +123,10 @@ function toggleTheme() {
   } else {
     setTheme("theme-lovely");
   }
+}
+
+function outputBoolean(bool) {
+  return (bool) ? "Yes":"No";
 }
 
 export default HomePage;
