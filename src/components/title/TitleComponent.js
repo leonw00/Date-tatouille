d78ffbb8596767ff7465a2d3c3c6cf68e5bd861b
Wynona Goldiella <57550160+wynonagcc@users.noreply.github.com>
2022-07-23 21:15:26 -0500

    Initial Generated Page setup (#24)

---
+++ src/components/title/TitleComponent.js
@@ -0,0 +1,15 @@
+import "./TitleComponent.css";
+
+function TitleComponent() {
+  return (
+    <div>
+      <h1>Date-tatouille</h1>
+      <h4>
+        We help generate date ideas for you, just like Remy helping Linguini in
+        Ratatouille!
+      </h4>
+    </div>
+  );
+}
+
+export default TitleComponent;
