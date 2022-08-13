972dc53c5b39482b43f522b671842bd04c1ee22f
wynonagcc <wynonagoldiella@gmail.com>
2022-08-13 13:50:30 -0500

    Updated Generated Page style

---
+++ src/components/generatorDice/GeneratorDice.js
@@ -0,0 +1,16 @@
+import "./GeneratorDice.css";
+
+function GeneratorDice() {
+  return (
+    <div>
+        <div className="base-dice">
+            <span className="dot"></span>
+            <span className="dot"></span>
+        </div>
+        <div className="tooltip">Regenerate Date Ideas!
+        </div>
+    </div>
+  );
+}
+
+export default GeneratorDice;
